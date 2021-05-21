be3babdd2215776894588ec29ad9eb256f2339bf
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-05-21 12:24:10 -0500

    clean up - needs custom button

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import CustomButton from './CustomButton'
+
 export default function Counter(props) {
     return (
         <div>
             <p>You clicked {props.count} times</p>
+            <CustomButton />
             <button onClick={() => props.setCount(props.count + 1)}>
                 Click me
             </button> 
